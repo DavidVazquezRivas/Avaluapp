680e91b6d439b40015618ab9eea21b9f0098aed1
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-02-28 17:47:58 +0100

    first commit

---
+++ api/src/main/java/com/uib/avaluapp/AvaluappApplication.java
@@ -0,0 +1,13 @@
+package com.uib.avaluapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaluappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaluappApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/uib/avaluapp/AvaluappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uib.avaluapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaluappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
