bdd7ddf49125ea5cc4193c6561c2c03b6ae0c1ec
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-04-13 01:04:43 +0200

    Auth exception codes
    Create exception codes to allow auth error handling

--- api/src/main/java/com/uib/avaluapp/global/exceptions/ExceptionCode.java
+++ api/src/main/java/com/uib/avaluapp/global/exceptions/ExceptionCode.java
@@ -9,7 +9,11 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionCode {
     DEFAULT("An error occurred", HttpStatus.INTERNAL_SERVER_ERROR),
     USER_NOT_FOUND("User not found", HttpStatus.NOT_FOUND),
-    USER_ALREADY_EXISTS("User already exists", HttpStatus.CONFLICT);
+    USER_ALREADY_EXISTS("User already exists", HttpStatus.CONFLICT),
+    INVALID_CREDENTIALS("Invalid username or password", HttpStatus.UNAUTHORIZED),
+    INVALID_REFRESH_TOKEN("Invalid refresh token", HttpStatus.UNAUTHORIZED),
+    INVALID_TOKEN("Invalid token", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED("Unauthorized", HttpStatus.UNAUTHORIZED);
 
     private final String message;
     private final HttpStatus code;
