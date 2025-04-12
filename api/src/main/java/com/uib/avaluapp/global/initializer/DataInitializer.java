3a55381a909eb08df6ebfbb5532d0c8df662adea
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-04-13 01:04:15 +0200

    Initialize data
    Initialize data to ensure the system always has at least an admin user

---
+++ api/src/main/java/com/uib/avaluapp/global/initializer/DataInitializer.java
@@ -0,0 +1,52 @@
+package com.uib.avaluapp.global.initializer;
+
+import com.uib.avaluapp.global.exceptions.BaseException;
+import com.uib.avaluapp.user.domain.models.Role;
+import com.uib.avaluapp.user.domain.services.UserService;
+import com.uib.avaluapp.user.infrastructure.web.requests.CreateUserRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+    private final UserService userService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.createAdminUser();
+        this.createDefaultUser();
+    }
+
+    private void createAdminUser() {
+        try {
+            CreateUserRequest request = CreateUserRequest
+                    .builder()
+                    .username("admin")
+                    .email("admin")
+                    .password("admin")
+                    .role(Role.ADMIN)
+                    .build();
+            userService.createUser(request);
+        } catch (BaseException e) {
+            // Managed exception, user already exists, do nothing
+        }
+
+    }
+
+    private void createDefaultUser() {
+        try {
+            CreateUserRequest request = CreateUserRequest
+                    .builder()
+                    .username("user")
+                    .email("user")
+                    .password("user")
+                    .role(Role.USER)
+                    .build();
+            userService.createUser(request);
+        } catch (BaseException e) {
+            // Managed exception, user already exists, do nothing
+        }
+    }
+}
