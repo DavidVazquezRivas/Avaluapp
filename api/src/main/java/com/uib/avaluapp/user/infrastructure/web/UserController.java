a8d160419e16e3885bde74a07a6517de25a7243c
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-04-13 23:25:19 +0200

    Create UserDtoMapper
    Use Mapper dependency instead of manually mapping

--- api/src/main/java/com/uib/avaluapp/user/infrastructure/web/UserController.java
+++ api/src/main/java/com/uib/avaluapp/user/infrastructure/web/UserController.java
@@ -4,14 +4,13 @@ import com.uib.avaluapp.global.insfrastructure.web.BaseController;
 import com.uib.avaluapp.global.insfrastructure.web.response.ApiResponse;
 import com.uib.avaluapp.user.domain.services.UserService;
 import com.uib.avaluapp.user.infrastructure.web.requests.CreateUserRequest;
-import com.uib.avaluapp.user.infrastructure.web.responses.UserDTO;
+import com.uib.avaluapp.user.infrastructure.web.responses.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,18 +23,18 @@ public class UserController extends BaseController {
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse<List<UserDTO>>> getUsers() {
-        return handle(() -> userService.getAllUsers().stream().map(UserDTO::fromUser).collect(Collectors.toList()), "Users retrieved successfully", HttpStatus.OK);
+    public ResponseEntity<ApiResponse<List<UserDto>>> getUsers() {
+        return handle(() -> UserDtoMapper.INSTANCE.toDtoList(userService.getAllUsers()), "Users retrieved successfully", HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<UserDTO>> getUserById(@PathVariable Long id) {
-        return handle(() -> UserDTO.fromUser(userService.getSingleUser(id)), "User retrieved successfully", HttpStatus.OK);
+    public ResponseEntity<ApiResponse<UserDto>> getUserById(@PathVariable Long id) {
+        return handle(() -> UserDtoMapper.INSTANCE.toDto(userService.getSingleUser(id)), "User retrieved successfully", HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<ApiResponse<UserDTO>> createUser(@RequestBody CreateUserRequest createUserRequest) {
-        return handle(() -> UserDTO.fromUser(userService.createUser(createUserRequest)), "User created successfully", HttpStatus.CREATED);
+    public ResponseEntity<ApiResponse<UserDto>> createUser(@RequestBody CreateUserRequest createUserRequest) {
+        return handle(() -> UserDtoMapper.INSTANCE.toDto(userService.createUser(createUserRequest)), "User created successfully", HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
--- api/src/main/java/com/uib/avaluapp/user/infrastructure/web/responses/UserDTO.java
+++ api/src/main/java/com/uib/avaluapp/user/infrastructure/web/responses/UserDTO.java
@@ -1,24 +1,14 @@
 package com.uib.avaluapp.user.infrastructure.web.responses;
 
 import com.uib.avaluapp.user.domain.models.Role;
-import com.uib.avaluapp.user.domain.models.User;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class UserDTO {
+public class UserDto {
     private Long id;
     private String username;
     private String email;
     private Role role;
-
-    public static UserDTO fromUser(User user) {
-        return UserDTO.builder()
-                .id(user.getId())
-                .username(user.getUsername())
-                .email(user.getEmail())
-                .role(user.getRole())
-                .build();
-    }
 }
