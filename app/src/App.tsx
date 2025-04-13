5cc8aeb88d9f217399657ebf2bccacfd88adb4fe
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-04-13 20:09:38 +0200

    Add BrowserRouting
    Add routing on App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,6 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { useThemeContext } from '@/theme/ThemeContextProvider'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const { theme } = useThemeContext()
@@ -7,6 +8,11 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={''} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
