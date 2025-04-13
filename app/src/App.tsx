750230a8630759d3c7a6dc6a31b0bca00a556ed1
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-04-13 16:48:49 +0200

    Theme
    Theme condfiguration and use of providers

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,14 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { useThemeContext } from '@/theme/ThemeContextProvider'
+
 function App() {
-  return <></>
+  const { theme } = useThemeContext()
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
+  )
 }
 
 export default App
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,16 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import App from './App.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { ThemeContextProvider } from '@/theme/ThemeContextProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
   </StrictMode>
 )
