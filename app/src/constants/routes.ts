7290790953fbc002ed08e0f2ef30a5b85a716f74
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-04-18 19:58:03 +0200

    Basic routes
    Define constants for basic app routing

---
+++ app/src/constants/routes.ts
@@ -0,0 +1,15 @@
+export const PublicRoutes = {
+  Login: '/login',
+}
+
+export const PrivateRoutes = {
+  Private: '/private',
+}
+
+export const UserRoutes = {
+  Base: '/user',
+}
+
+export const AdminRoutes = {
+  Base: '/admin',
+}
