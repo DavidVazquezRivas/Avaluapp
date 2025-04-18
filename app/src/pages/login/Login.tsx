d25503e52d5faf769e58c81b21f5006da9e675b5
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-04-18 19:54:13 +0200

    Delete session on login page
    If the user gets to login page, it's logged out

--- app/src/pages/login/Login.tsx
+++ app/src/pages/login/Login.tsx
@@ -1,7 +1,13 @@
 import Layout from '@/components/layout/Layout'
 import SignIn from '@/components/lib/signin/SignIn'
+import { deleteSession } from '@/utils/session.utils'
+import { useEffect } from 'react'
 
 const Login: React.FC = () => {
+  useEffect(() => {
+    deleteSession()
+  })
+
   return (
     <Layout>
       <SignIn />
