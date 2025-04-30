cb86e1f2a9c4f2ea8e60bf867d76828b2f7c9357
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-05-01 01:49:13 +0200

    User basic page
    Created user management basic page

--- app/src/pages/admin/Admin.tsx
+++ app/src/pages/admin/Admin.tsx
@@ -3,13 +3,15 @@ import { AdminRoutes } from '@/constants/routes'
 import { Navigate, Route } from 'react-router-dom'
 import { lazy } from 'react'
 
-const AdminDashboard = lazy(() => import('./pages/AdminDashboard'))
+const AdminDashboard = lazy(() => import('./pages/dashboard/AdminDashboard'))
+const UsersPage = lazy(() => import('./pages/users/UsersPage'))
 
 export const Admin = () => {
   return (
     <RoutesWithNotFound>
       <Route path='/' element={<Navigate to={AdminRoutes.Dashboard} />} />
       <Route path={AdminRoutes.Dashboard} element={<AdminDashboard />} />
+      <Route path={AdminRoutes.Users} element={<UsersPage />} />
     </RoutesWithNotFound>
   )
 }
---
+++ app/src/pages/admin/pages/users/UsersPage.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/layout/Layout'
+import UsersGrid from './components/grid/UsersGrid'
+
+export const UsersPage = () => {
+  return (
+    <Layout>
+      <UsersGrid />
+    </Layout>
+  )
+}
+
+export default UsersPage
