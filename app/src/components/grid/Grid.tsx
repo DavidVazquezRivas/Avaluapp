bbbd99ab763aaab7f30995c2aceddcf88753c308
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-05-02 01:24:09 +0200

    Allow Create Button on global Grid
    Crated a Create Button functionality on reusable Grid, also make filters responsive

--- app/src/components/grid/Grid.tsx
+++ app/src/components/grid/Grid.tsx
@@ -1,37 +1,44 @@
+import FilterComponent from './FiltersComponent'
 import {
   DEFAULT_PAGE_SIZE,
   DEFAULT_PAGE_SIZE_OPTIONS,
 } from '@/constants/grid.constants'
 import { useGridLocaleText } from '@/hooks/useGridLocaleText'
 import { DefaultFilter } from '@/models/defaultfilter.model'
-import { Button, Stack } from '@mui/material'
+import { Box, Button, Stack } from '@mui/material'
 import { DataGrid, DataGridProps, useGridApiRef } from '@mui/x-data-grid'
 
 interface GridProps extends DataGridProps {
   defaultFilters?: DefaultFilter[]
+  createButton?: {
+    label: string
+    onClick: () => void
+  }
 }
 
-export const Grid: React.FC<GridProps> = ({ defaultFilters, ...props }) => {
+export const Grid: React.FC<GridProps> = ({
+  defaultFilters,
+  createButton,
+  ...props
+}) => {
   const apiRef = useGridApiRef()
   const localeText = useGridLocaleText()
+  const CreateButton = createButton && (
+    <Button variant='contained' onClick={createButton.onClick}>
+      {createButton.label}
+    </Button>
+  )
 
   return (
-    <Stack direction='column' spacing={2} width='100%' height='100%'>
-      <Stack direction='row' spacing={2} pl={2} pr={2}>
-        {defaultFilters &&
-          defaultFilters.map(({ label, filterModel }) => {
-            return (
-              <Button
-                key={label}
-                variant='outlined'
-                sx={{ color: 'text.primary' }}
-                onClick={() => apiRef.current?.setFilterModel(filterModel)}>
-                {label}
-              </Button>
-            )
-          })}
-      </Stack>
-
+    <Stack direction='column' spacing={3} width='100%' height='100%'>
+      <Box
+        display='grid'
+        gridTemplateColumns='1fr auto'
+        gap={2}
+        alignItems='flex-end'>
+        <FilterComponent apiRef={apiRef} defaultFilters={defaultFilters} />
+        {CreateButton}
+      </Box>
       <DataGrid
         apiRef={apiRef}
         localeText={localeText}
