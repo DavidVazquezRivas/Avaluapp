973cff782049a0a98d4eb26b183ae95a55f62b9b
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-07-06 18:41:45 +0200

    Define survey DTO

---
+++ api/src/main/java/com/uib/avaluapp/surveys/infrastructure/web/responses/SurveyDto.java
@@ -0,0 +1,18 @@
+package com.uib.avaluapp.surveys.infrastructure.web.responses;
+
+import com.uib.avaluapp.project.infrastructure.web.responses.ProjectDto;
+import com.uib.avaluapp.user.infrastructure.web.responses.UserDto;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SurveyDto {
+    private Long id;
+    private String name;
+    private String createdAt;
+    private String urlCode;
+    private String status;
+    private UserDto lead;
+    private ProjectDto project;
+}
