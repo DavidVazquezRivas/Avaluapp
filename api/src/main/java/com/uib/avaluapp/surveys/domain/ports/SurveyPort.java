aceab120fba5e00d05df5114e77df16c90bbe6b4
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-07-06 18:42:43 +0200

    Survey port and service interfaces

---
+++ api/src/main/java/com/uib/avaluapp/surveys/domain/ports/SurveyPort.java
@@ -0,0 +1,19 @@
+package com.uib.avaluapp.surveys.domain.ports;
+
+import com.uib.avaluapp.surveys.domain.models.Survey;
+
+import java.util.List;
+
+public interface SurveyPort {
+    Survey fetchLead(Survey survey);
+
+    Survey fetchProject(Survey survey);
+
+    List<Survey> getAllSurveysByProjectId(Long projectId);
+
+    Survey createSurvey(Survey survey);
+
+    void deleteSurvey(Long surveyId);
+
+    Survey updateSurvey(Survey survey);
+}
