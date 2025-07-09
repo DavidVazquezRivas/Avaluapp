b26ecab79c937b4f9810b3888c3b1b368274bd47
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-07-09 22:47:58 +0200

    Add SurveyGrid and SurveyForm components for survey management with CRUD functionality

---
+++ app/src/pages/admin/pages/projectdetail/components/grids/SurveyGrid.tsx
@@ -0,0 +1,165 @@
+import getAllSurveysQueryOptions from '../../queries/survey/getAll.survey.query'
+import createSurveyQueryOptions from '../../queries/survey/create.survey.query'
+import deleteSurveyQueryOptions from '../../queries/survey/delete.survey.query'
+import DeleteIcon from '@mui/icons-material/Delete'
+import EditIcon from '@mui/icons-material/Edit'
+import { usePanel } from '@/contexts/PanelContext'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useTranslation } from 'react-i18next'
+import { PanelType } from '@/models/panels.model'
+import { Survey } from '../../models/survey.model'
+import { useMemo } from 'react'
+import { GridActionsCellItem, GridColDef } from '@mui/x-data-grid'
+import { User } from '@/models/user.model'
+import { dateRenderer } from '@/utils/renderers/date.renderer'
+import Grid from '@/components/grid/Grid'
+import { SurveyForm } from '../panels/SurveyForm'
+import updateSurveyQueryOptions from '../../queries/survey/create.survey.query'
+
+interface SurveyGridProps {
+  projectId: number
+}
+
+export const SurveyGrid: React.FC<SurveyGridProps> = ({ projectId }) => {
+  const { t } = useTranslation()
+  const { openPanel } = usePanel()
+  const queryClient = useQueryClient()
+
+  const { data, isLoading, isFetching } = useQuery(
+    getAllSurveysQueryOptions(projectId)
+  )
+
+  const createMutation = useMutation(createSurveyQueryOptions(queryClient))
+
+  const deleteMutation = useMutation(deleteSurveyQueryOptions(queryClient))
+
+  const updateMutation = useMutation(updateSurveyQueryOptions(queryClient))
+
+  const handleCreate = async (formData: FormData) => {
+    const survey = {
+      name: formData.get('name') as string,
+      projectId: projectId,
+      leadId: formData.get('leadId') as unknown as number,
+    }
+
+    await createMutation.mutateAsync(survey)
+  }
+
+  const onClickCreate = () => {
+    openPanel({
+      type: PanelType.FormDialog,
+      title: t('admin.projectdetail.tabs.surveys.grid.actions.create.title'),
+      content: <SurveyForm />,
+      onSubmit: handleCreate,
+      errorText: t(
+        'admin.projectdetail.tabs.surveys.grid.actions.create.error'
+      ),
+    })
+  }
+
+  const handleDelete = (id: number, name: string) => {
+    openPanel({
+      type: PanelType.ConfirmDialog,
+      title: t(
+        'admin.projectdetail.tabs.surveys.grid.actions.delete.confirm.title'
+      ),
+      text: t(
+        'admin.projectdetail.tabs.surveys.grid.actions.delete.confirm.text',
+        { name }
+      ),
+      confirmText: t(
+        'admin.projectdetail.tabs.surveys.grid.actions.delete.confirm.button.ok'
+      ),
+      cancelText: t(
+        'admin.projectdetail.tabs.surveys.grid.actions.delete.confirm.button.cancel'
+      ),
+      onConfirm: () => deleteMutation.mutate(id),
+    })
+  }
+
+  const handleUpdate = async (formData: FormData) => {
+    const updatedSurvey = {
+      id: formData.get('id') as unknown as number,
+      name: formData.get('name') as string,
+      projectId: projectId,
+      leadId: formData.get('leadId') as unknown as number,
+    }
+
+    await updateMutation.mutateAsync(updatedSurvey)
+  }
+
+  const onClickUpdate = (survey: Survey) => {
+    openPanel({
+      type: PanelType.FormDialog,
+      title: t('admin.projectdetail.tabs.surveys.grid.actions.edit.title'),
+      content: <SurveyForm survey={survey} />,
+      onSubmit: handleUpdate,
+      errorText: t('admin.projectdetail.tabs.surveys.grid.actions.edit.error'),
+    })
+  }
+
+  const columns: GridColDef<Survey>[] = useMemo(
+    () => [
+      {
+        field: 'name',
+        headerName: t('admin.projectdetail.tabs.surveys.grid.columns.name'),
+        flex: 1,
+      },
+      {
+        field: 'lead',
+        headerName: t('admin.projectdetail.tabs.surveys.grid.columns.lead'),
+        valueGetter: (value: User) => value.username,
+        flex: 1,
+      },
+      {
+        field: 'urlCode',
+        headerName: t('admin.projectdetail.tabs.surveys.grid.columns.urlCode'),
+        flex: 1,
+      },
+      {
+        field: 'createdAt',
+        headerName: t(
+          'admin.projectdetail.tabs.surveys.grid.columns.createdAt'
+        ),
+        flex: 1,
+        valueGetter: (value: string) => dateRenderer(new Date(value)),
+      },
+      {
+        field: 'actions',
+        type: 'actions',
+        minWidth: 140,
+        getActions: (params: { row: Survey }) => [
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            onClick={() => handleDelete(params.row.id, params.row.name)}
+            label={t(
+              'admin.projectdetail.tabs.surveys.grid.actions.delete.label'
+            )}
+          />,
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            onClick={() => onClickUpdate(params.row)}
+            label={t(
+              'admin.projectdetail.tabs.surveys.grid.actions.edit.label'
+            )}
+          />,
+        ],
+      },
+    ],
+    [t]
+  )
+
+  return (
+    <Grid
+      isLoading={isLoading || isFetching}
+      rows={data ?? []}
+      columns={columns}
+      createButton={{
+        onClick: onClickCreate,
+        label: t('admin.projectdetail.tabs.surveys.grid.actions.create.label'),
+      }}
+    />
+  )
+}
+
+export default SurveyGrid
---
+++ app/src/pages/admin/pages/projectdetail/components/panels/SurveyForm.tsx
@@ -0,0 +1,93 @@
+import { useTranslation } from 'react-i18next'
+import { Survey } from '../../models/survey.model'
+import {
+  Autocomplete,
+  FormControl,
+  FormLabel,
+  Stack,
+  TextField,
+} from '@mui/material'
+import { User } from '@/models/user.model'
+import { useMemo, useState } from 'react'
+import store from '@/redux/store'
+import { Role } from '@/models/role.model'
+
+interface SurveyFormProps {
+  survey?: Survey
+}
+
+export const SurveyForm: React.FC<SurveyFormProps> = ({ survey }) => {
+  const { t } = useTranslation()
+
+  const [selectedLeadId, setSelectedLeadId] = useState<number>(
+    survey?.lead?.id ?? 0
+  )
+
+  const users = store.getState().user.user
+  const leads: User[] = useMemo(
+    () => users.filter((u) => u.role === Role.User),
+    [users]
+  )
+
+  return (
+    <Stack direction='column' spacing={3} width='100%' height='100%'>
+      <input type='hidden' name='leadId' value={selectedLeadId} />
+
+      <FormControl sx={{ display: 'none' }}>
+        <FormLabel htmlFor='id'>
+          {t('admin.projectdetail.tabs.surveys.form.fields.id.label')}
+        </FormLabel>
+        <TextField
+          id='id'
+          name='id'
+          fullWidth
+          variant='outlined'
+          defaultValue={survey?.id}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel htmlFor='name'>
+          {t('admin.projectdetail.tabs.surveys.form.fields.name.label')}
+        </FormLabel>
+        <TextField
+          id='name'
+          name='name'
+          placeholder={t(
+            'admin.projectdetail.tabs.surveys.form.fields.name.placeholder'
+          )}
+          autoFocus
+          required
+          fullWidth
+          variant='outlined'
+          defaultValue={survey?.name}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel htmlFor='lead'>
+          {t('admin.projectdetail.tabs.surveys.form.fields.lead.label')}
+        </FormLabel>
+        <Autocomplete
+          id='lead'
+          options={leads}
+          getOptionLabel={(option) => option.username}
+          defaultValue={leads.find((l) => l.id === survey?.lead?.id) || null}
+          onChange={(_, value) => {
+            setSelectedLeadId(value?.id ?? 0)
+          }}
+          isOptionEqualToValue={(option, value) => option.id === value.id}
+          sx={{ '& .MuiAutocomplete-endAdornment': { display: 'none' } }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              placeholder={t(
+                'admin.projectdetail.tabs.surveys.form.fields.lead.placeholder'
+              )}
+              variant='outlined'
+              fullWidth
+            />
+          )}
+        />
+      </FormControl>
+    </Stack>
+  )
+}
