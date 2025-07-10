db435b060c899b5946ebf4034185ad318edcb28c
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-07-10 20:10:14 +0200

    Add SurveyPage component and update User routes

--- app/src/pages/user/User.tsx
+++ app/src/pages/user/User.tsx
@@ -3,13 +3,15 @@ import { UserRoutes } from '@/constants/routes'
 import { Navigate, Route } from 'react-router-dom'
 import { lazy } from 'react'
 
-const UserDashboard = lazy(() => import('./pages/UserDashboard'))
+const UserDashboard = lazy(() => import('./pages/dashboard/UserDashboard'))
+const SurveyPage = lazy(() => import('./pages/survey/SurveyPage'))
 
 export const User = () => {
   return (
     <RoutesWithNotFound>
       <Route path='/' element={<Navigate to={UserRoutes.Dashboard} />} />
       <Route path={UserRoutes.Dashboard} element={<UserDashboard />} />
+      <Route path={UserRoutes.Survey} element={<SurveyPage />} />
     </RoutesWithNotFound>
   )
 }
