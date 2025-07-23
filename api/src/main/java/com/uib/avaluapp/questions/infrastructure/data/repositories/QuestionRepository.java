e4f8108629d0a3695b1e4ae1d26ba8f91c6949be
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-07-23 21:10:26 +0200

    Define Question and Option repositories

---
+++ api/src/main/java/com/uib/avaluapp/questions/infrastructure/data/repositories/QuestionRepository.java
@@ -0,0 +1,16 @@
+package com.uib.avaluapp.questions.infrastructure.data.repositories;
+
+import com.uib.avaluapp.questions.infrastructure.data.models.QuestionEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QuestionRepository extends CrudRepository<QuestionEntity, Long> {
+    @Query("SELECT q FROM QuestionEntity q LEFT JOIN FETCH q.options WHERE q.id = :id")
+    Optional<QuestionEntity> findWithOptions(@Param("id") Long id);
+
+    List<QuestionEntity> findAllByProjectId(Long projectId);
+}
