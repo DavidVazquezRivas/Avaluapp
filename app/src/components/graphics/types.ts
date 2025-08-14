9af920c76e55e34cfdd8b688e80c2dc7f7e0c3fe
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-08-14 19:13:04 +0200

    feat: add types for numeric and chart data models

---
+++ app/src/components/graphics/types.ts
@@ -0,0 +1,31 @@
+import { BaseAnswer } from '@/models/answer.model'
+
+export interface NumericAnswer extends Omit<BaseAnswer, 'value'> {
+  value: number
+}
+
+export interface TimelineData {
+  date: Date
+  count: number
+}
+
+export interface PieChartData {
+  label: string
+  value: number
+  count: number
+}
+
+export interface BarChartData {
+  label: string
+  value: number
+  order?: number
+}
+
+export interface BoxPlotData {
+  min: number
+  q1: number
+  median: number
+  q3: number
+  max: number
+  outliers?: number[]
+}
--- app/src/models/answer.model.ts
+++ app/src/models/answer.model.ts
@@ -2,3 +2,8 @@ export interface SubmitAnswer {
   questionId: string
   answer: string
 }
+
+export interface BaseAnswer {
+  value: string | number
+  answeredAt: Date | string
+}
