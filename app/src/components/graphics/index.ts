f74f53972594a75db17db132c1d26552e9a997be
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-08-14 19:13:48 +0200

    feat: create index file for graphic components and types export

---
+++ app/src/components/graphics/index.ts
@@ -0,0 +1,18 @@
+// Graphic components
+export { Timeline } from './Timeline'
+export { PieChart } from './PieChart'
+export { BarChart } from './BarChart'
+export { BoxPlot } from './BoxPlot'
+export { NumberDisplay } from './NumberDisplay'
+
+// Base component
+export { BaseGraphicCard } from './BaseGraphicCard'
+
+// Types
+export type {
+  NumericAnswer,
+  TimelineData,
+  PieChartData,
+  BarChartData,
+  BoxPlotData,
+} from './types'
