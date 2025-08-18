78e601dff4aa16045399fb925594dd2f6e44eda6
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-08-18 19:18:35 +0200

    feat: update BaseAnswer interface to allow array values and enhance Result interface structure

--- app/src/models/answer.model.ts
+++ app/src/models/answer.model.ts
@@ -1,9 +1,16 @@
+import { Question } from './question.model'
+
 export interface SubmitAnswer {
   questionId: string
   answer: string
 }
 
 export interface BaseAnswer {
-  value: string | number
+  value: string | number | Array<string | number>
   answeredAt: Date | string
+}
+
+export interface Result {
+  answers: BaseAnswer[]
+  question: Question
 }
