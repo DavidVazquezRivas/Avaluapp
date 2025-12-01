8ddc3d0729bdf9cd6df55c2ba51c29409d891a42
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2025-12-01 21:06:32 +0100

    Clear initial data for real production system

--- api/src/main/java/com/uib/avaluapp/global/initializer/DataInitializer.java
+++ api/src/main/java/com/uib/avaluapp/global/initializer/DataInitializer.java
@@ -5,48 +5,48 @@ import com.uib.avaluapp.user.domain.models.Role;
 import com.uib.avaluapp.user.domain.services.UserService;
 import com.uib.avaluapp.user.infrastructure.web.requests.CreateUserRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
+    
     private final UserService userService;
+
+    // Inject the values from application.properties
+    @Value("${app.admin.username}")
+    private String adminUsername;
+
+    @Value("${app.admin.email}")
+    private String adminEmail;
+
+    @Value("${app.admin.password}")
+    private String adminPassword;
 
     @Override
     public void run(String... args) throws Exception {
         this.createAdminUser();
-        this.createDefaultUser();
+        // createDefaultUser() eliminado
     }
 
     private void createAdminUser() {
         try {
             CreateUserRequest request = CreateUserRequest
                     .builder()
-                    .username("admin")
-                    .email("admin")
-                    .password("admin")
+                    .username(adminUsername)
+                    .email(adminEmail)
+                    .password(adminPassword)
                     .role(Role.ADMIN)
                     .build();
+            
             userService.createUser(request, null);
+            System.out.println("--- ADMIN USER CREATED: " + adminUsername + " ---");
+            
         } catch (BaseException e) {
             // Managed exception, user already exists, do nothing
-        }
-
-    }
-
-    private void createDefaultUser() {
-        try {
-            CreateUserRequest request = CreateUserRequest
-                    .builder()
-                    .username("user")
-                    .email("user")
-                    .password("user")
-                    .role(Role.USER)
-                    .build();
-            userService.createUser(request, null);
-        } catch (BaseException e) {
-            // Managed exception, user already exists, do nothing
+            System.out.println("--- ADMIN USER ALREADY EXISTS ---");
         }
     }
 }
